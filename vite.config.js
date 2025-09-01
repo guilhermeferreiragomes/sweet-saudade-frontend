6939a7a945186015226bc3000e61687a54f4cac0
Guilherme Ferreira Gomes <gffgs@iscte-iul.pt>
2025-09-01 20:43:02 +0100

    Remove proxy from vite config - frontend ready

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000"
-    }
-  }
+  plugins: [react()]
 })
