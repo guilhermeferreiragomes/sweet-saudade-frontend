d9becb85f91286b0bf206c7671da03512a44e553
Guilherme Ferreira Gomes <gffgs@iscte-iul.pt>
2025-09-07 18:51:22 +0100

    commit

--- src/components/Common/Cookies/Cookies.jsx
+++ src/components/Common/Cookies/Cookies.jsx
@@ -1,17 +1,16 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import CookieConsent from "react-cookie-consent";
+import './Cookies.css';
 
 const Cookies = () => {
   
   const handleAccept = () => {
     localStorage.setItem('cookies-accepted', 'true');
-    // Recarregar para ativar funcionalidades
     window.location.reload();
   };
 
   const handleDecline = () => {
     localStorage.setItem('cookies-accepted', 'false');
-    // Recarregar para desativar funcionalidades
     window.location.reload();
   };
 
@@ -21,38 +20,20 @@ const Cookies = () => {
             location="bottom"
             buttonText="Aceitar"
             declineButtonText="Rejeitar"
-            declineButtonStyle={{ 
-              background: "#4e503b", 
-              color: "#fff",  
-              fontSize: "14px", 
-              borderRadius: "5px", 
-              padding: "10px 20px", 
-              marginLeft: "10px" 
-            }}
             cookieName="CookieConsent"
-            style={{ 
-              background: "#262724", 
-              height: "80px", 
-              display: "flex", 
-              alignItems: "center", 
-              fontSize: "17px" 
-            }}
-            buttonStyle={{ 
-              background:"#EBB104", 
-              color: "#4e503b", 
-              fontSize: "14px", 
-              borderRadius: "5px", 
-              padding: "10px 20px" 
-            }}
-            expires={150}
-            enableDeclineButton
             onAccept={handleAccept}
             onDecline={handleDecline}
+            enableDeclineButton
+            expires={150}
+            buttonWrapperClasses="cookie-buttons-wrapper"
+            contentClasses="cookie-content"
         >
-           🍪 Utilizamos cookies para melhorar a sua experiência, segurança e análise do tráfego.{" "} 
-           <a href="/politica-privacidade" style={{color: '#EBB104', textDecoration: 'underline', fontSize: '14px'}}>
-             Saiba mais
-           </a>
+           <span className="cookie-text">
+             🍪 Utilizamos cookies para melhorar a sua experiência, segurança e análise do tráfego.{" "}
+             <a href="/politica-privacidade">
+               Saiba mais
+             </a>
+           </span>
         </CookieConsent>
     </div>
   )
