bb94830a38e02128c1e8455d9877fed9bc50c4a9
Guilherme Ferreira Gomes <gffgs@iscte-iul.pt>
2025-09-13 15:35:19 +0100

    commit

--- src/components/Common/Navbar/Navbar.jsx
+++ src/components/Common/Navbar/Navbar.jsx
@@ -22,16 +22,17 @@ const Navbar = () => {
             <button className="lang-btn">EN</button>
         </div>
         
-        <div className="hamburger" onClick={toggleMenu}>
+        <div className={`hamburger ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
           <span></span>
           <span></span>
           <span></span>
         </div>
 
         <ul className={`nav-links ${isMenuOpen ? 'nav-active' : ''}`}>
-            <li><Link to="/produtos"><h3>PRODUTOS</h3></Link></li>
-            <li><Link to="/sobre-nos"><h3>SOBRE NÓS</h3></Link></li>
-            <li><Link to="/contactos"><h3>CONTACTOS</h3></Link></li>
+            <li><Link to="/" onClick={toggleMenu}><h3>INÍCIO</h3></Link></li>
+            <li><Link to="/produtos" onClick={toggleMenu}><h3>PRODUTOS</h3></Link></li>
+            <li><Link to="/sobre-nos" onClick={toggleMenu}><h3>SOBRE NÓS</h3></Link></li>
+            <li><Link to="/contactos" onClick={toggleMenu}><h3>CONTACTOS</h3></Link></li>
         </ul>
 
 
