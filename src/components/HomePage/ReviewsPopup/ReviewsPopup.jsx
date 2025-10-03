5ec3dcf5f0c0e71b98a832cf332ea1df30194c84
Guilherme Ferreira Gomes <gffgs@iscte-iul.pt>
2025-10-03 17:27:00 +0100

    falta adiconar o excel

--- src/components/HomePage/ReviewsPopup/ReviewsPopup.jsx
+++ src/components/HomePage/ReviewsPopup/ReviewsPopup.jsx
@@ -2,21 +2,50 @@ import { React, useState } from 'react'
 import Modal from 'react-modal';
 import './ReviewsPopup.css'
 
-const ReviewsPopup = () => {
+// É importante definir o elemento raiz da aplicação para acessibilidade
+Modal.setAppElement('#root'); // Ajuste para o ID do seu elemento raiz se for diferente
 
-    const [visible, setVisible] = useState(false)
+const ReviewsPopup = () => {
+  const [visible, setVisible] = useState(false)
+
+  const customStyles = {
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.75)', // Background escuro semi-transparente
+      zIndex: 1000
+    },
+  };
 
   return (
     <div>
       <div className='form-popup'>
-      <button onClick={() => setVisible(true)} className='popup-button'>
-        <p>Deixe-nos a sua opinião!</p>
-      </button>
-      <Modal isOpen={visible} onRequestClose={() => setVisible(false)}>
-        <form>
-            
-        </form>
-      </Modal>
+        <button onClick={() => setVisible(true)} className='popup-button'>
+          <p>Deixe-nos a sua opinião!</p>
+        </button>
+        <Modal className="formulario"
+          isOpen={visible}
+          onRequestClose={() => setVisible(false)}
+          style={customStyles}
+        >
+            <form className="reviews-form">
+              <div className="reviews-form-group">
+                <label className="reviews-label">Nome</label>
+                <input type='text' className="reviews-input" />
+              </div>
+              <div className="reviews-form-group">
+                <label className="reviews-label">Email</label>
+                <input type='email' className="reviews-input" />
+              </div>
+              <div className="reviews-form-group">
+                <label className="reviews-label">Mensagem</label>
+                <textarea className="reviews-textarea" />
+              </div>
+              <div className="reviews-form-group">
+                <button type="submit" className="reviews-submit-btn">
+                  Enviar
+                </button>
+              </div>
+            </form>
+        </Modal>
       </div>
     </div>
   )
