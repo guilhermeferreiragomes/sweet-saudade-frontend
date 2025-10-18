48c0bcaff318c3845665eb960a78911b2768af08
Guilherme Ferreira Gomes <gffgs@iscte-iul.pt>
2025-10-18 20:12:43 +0100

    commit

--- src/components/Produtcs/ProductDetail/ProductDetail.jsx
+++ src/components/Produtcs/ProductDetail/ProductDetail.jsx
@@ -7,17 +7,18 @@ import Footer from '../../Common/Footer/Footer'
 import { Link } from 'react-router-dom'
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 
-
 const ProductDetail = () => {
   const { slug } = useParams()
   const product = productsData.find(item => item.slug === slug)
-
+  
   if (!product) {
     return <div>Product not found</div>
   }
+  
+  // Verificar se o produto tem opções de pacote
+  const hasPackOptions = product.packOptions && product.packOptions.length > 0;
 
   return (
-
     <div className='product-detail-container'>
       <Navbar />
       <div className='go-back-btn'>
@@ -28,16 +29,38 @@ const ProductDetail = () => {
       </div>
       <div className='product-detail'>
         <img src={product.image} alt={product.name} className='product-image' />
-          <div className='product-information'>
-            <h2 className='product-name'>{product.name}</h2>
-            <p className='product-price'>{product.price}</p>
-            <p className='product-description'>{product.description}</p>
-            <p className='product-pack'>{product.pack}</p>
-            <Link to="/contactos">
-              <button className='add-to-cart'>ENCOMENDAR</button>
-            </Link>
-          </div>
+        <div className='product-information'>
+          <h2 className='product-name'>{product.name}</h2>
+          
+          {/* Lista de opções de pacote disponíveis */}
+          {hasPackOptions && (
+            <div className="product-pack-options">
+              <h3 className="pack-options-title">Opções disponíveis:</h3>
+              <div className="pack-options-list">
+                {product.packOptions.map((option, index) => (
+                  <div key={index} className="pack-option-item">
+                    <span className="pack-name">{option.pack}</span>
+                    <span className="pack-price">{option.price}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+          
+          <Link to="/contactos">
+            <button className='add-to-cart'>ENCOMENDAR</button>
+          </Link>
+        </div>
       </div>
+      
+      {/* Descrição do produto movida para fora do layout principal */}
+      <div className="product-description-wrapper">
+        <div className="product-description-container">
+          <h2 className="description-title">Sobre o Produto</h2>
+          <p className="product-description">{product.description}</p>
+        </div>
+      </div>
+      
       <Footer />
     </div>
   )
--- src/components/Produtcs/ProductsList/ProductsList.jsx
+++ src/components/Produtcs/ProductsList/ProductsList.jsx
@@ -8,19 +8,31 @@ const ProductsList = () => {
     <div className='ProductsList-container'>
         <div className='ProductsList'>
             <div className='Product'>
-                {productsData.map((product) => (
-                    <div key={product.id} className='Product-card'>
-                        <Link to={`/produtos/${product.slug}`}>
-                            <img src={product.image} alt={product.name} className='Product-image' />
-                            <h3 className='Product-name'>{product.name}</h3>
-                            <p className='Product-pack'>{product.pack}</p>
-                            <h4 className='Product-price'>{product.price}</h4>
-                        </Link>
-                        <Link to={`/contactos`}>
-                            <button className='Add-to-cart-button'>ENCOMENDAR</button>
-                        </Link>
-                    </div>
-                ))}   
+                {productsData.map((product) => {
+                    // Obter o segundo pack (índice 1) se existir, senão usar o primeiro
+                    const packOption = product.packOptions && product.packOptions.length > 1 
+                        ? product.packOptions[1]
+                        : product.packOptions && product.packOptions.length > 0
+                            ? product.packOptions[0]
+                            : null;
+                    
+                    const packText = packOption ? packOption.pack : product.pack || '';
+                    const priceText = packOption ? packOption.price : product.defaultPrice || product.price || '';
+                    
+                    return (
+                        <div key={product.id} className='Product-card'>
+                            <Link to={`/produtos/${product.slug}`}>
+                                <img src={product.image} alt={product.name} className='Product-image' />
+                                <h3 className='Product-name'>{product.name}</h3>
+                                <p className='Product-pack'>{packText}</p>
+                                <h4 className='Product-price'>{priceText}</h4>
+                            </Link>
+                            <Link to={`/contactos`}>
+                                <button className='Add-to-cart-button'>ENCOMENDAR</button>
+                            </Link>
+                        </div>
+                    );
+                })}   
             </div>
         </div>
     </div>
